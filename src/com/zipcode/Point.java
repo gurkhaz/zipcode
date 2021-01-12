74dd1ec5ca279c4b8d260be3dfe13086d1b04807
gurkhaz <sanjayshrestha31@gmail.com>
2021-01-11 21:24:20 -0500

    created the point class

---
+++ src/com/zipcode/Point.java
@@ -0,0 +1,30 @@
+package com.zipcode;
+
+public class Point {
+	Integer x;
+	Integer y;
+
+	public Point(Integer x, Integer y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point() {
+	}
+
+	public Integer getX() {
+		return x;
+	}
+
+	public void setX(Integer x) {
+		this.x = x;
+	}
+
+	public Integer getY() {
+		return y;
+	}
+
+	public void setY(Integer y) {
+		this.y = y;
+	}
+}
